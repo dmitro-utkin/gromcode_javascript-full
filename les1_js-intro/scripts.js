be9d2929757bb98bbb17d42b47305b5a9313570c
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-02-14 10:01:01 +0200

    les1 done

---
+++ les1_js-intro/scripts.js
@@ -0,0 +1,13 @@
+// var a = 1;
+// const b = 2;
+// let c = 3;
+// c = 4;
+// console.log(c);
+// alert(b);
+
+const c = 4;
+const d = 5;
+const e = c + d;
+console.log(e);
+
+document.querySelector('a').innerText = 'Hello world!';
