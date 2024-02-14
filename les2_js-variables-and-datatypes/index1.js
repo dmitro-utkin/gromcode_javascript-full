ca5f63504fbb8678c19995c86e5ab9fee53b7768
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-02-14 13:18:41 +0200

    les2 done

---
+++ les2_js-variables-and-datatypes/index1.js
@@ -0,0 +1,15 @@
+const userName = 'John';
+const greeting1 = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
+
+const personName = 'John';
+
+// let age = 17;
+// age = 77;
+
