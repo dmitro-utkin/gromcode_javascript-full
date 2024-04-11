2ac9416fe581176845414ff5dfda3e68624ecb4b
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-04-11 10:23:36 +0300

    try to do hometask3 with todolist and localStorage

---
+++ les27_js-browser-local-storage/task3(todolist_with_localstorage)/scripts/index.js
@@ -0,0 +1,7 @@
+import { initTodoListHandlers } from "./todoList.js";
+import { renderTasks } from "./renderer.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderTasks();
+  initTodoListHandlers();
+});
