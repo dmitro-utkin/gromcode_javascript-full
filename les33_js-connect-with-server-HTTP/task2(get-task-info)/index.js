8925242edab682a683aca98d81bb1112cc5bde01
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-04-19 10:14:11 +0300

    try to do task2

---
+++ les33_js-connect-with-server-HTTP/task2(get-task-info)/index.js
@@ -0,0 +1,32 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+function getTasksList() {
+  // put your code here
+  return fetch(baseUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Failed to fetch tasks');
+  });
+}
+
+function getTaskById(taskId) {
+  // put your code here
+    return fetch(`${baseUrl}/${taskId}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Failed to fetch task by ID');
+    });
+}
+
+// examples
+getTasksList().then(tasksList => {
+  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+});
+
+getTaskById('2').then(taskData => {
+  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+});
