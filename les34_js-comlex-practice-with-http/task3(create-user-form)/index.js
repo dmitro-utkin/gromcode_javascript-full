3936cec84a0ca780d7ac7c90357c6a238b5fc4c9
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-04-24 12:40:15 +0300

    task3 changes with the mentoring solution

--- les34_js-comlex-practice-with-http/task3(create-user-form)/index.js
+++ les34_js-comlex-practice-with-http/task3(create-user-form)/index.js
@@ -1,41 +1,103 @@
-const serverUrl = 'https://662630a1052332d55321f809.mockapi.io/api/v1/users';
+// const serverUrl = 'https://662630a1052332d55321f809.mockapi.io/api/v1/users';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.login-form');
-  const submitButton = form.querySelector('.submit-button');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const form = document.querySelector('.login-form');
+//   const submitButton = form.querySelector('.submit-button');
 
-  form.addEventListener('input', function () {
-    submitButton.disabled = !form.reportValidity();
+//   form.addEventListener('input', function () {
+//     submitButton.disabled = !form.reportValidity();
+//   });
+
+//   form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     if (!form.reportValidity()) {
+//       return;
+//     }
+
+//     const formData = Object.fromEntries(new FormData(form));
+//     return fetch(serverUrl, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(formData),
+//     })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         alert(JSON.stringify(data, null, 2));
+//         form.reset();
+//         submitButton.disabled = true;
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   });
+// });
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+const emailInput = document.querySelector('[name="email"]');
+const passwordInput = document.querySelector('[name="password"]');
+const nameInput = document.querySelector('[name="name"]');
+
+const formElem = document.querySelector('.login-form');
+const submitBtnElem = document.querySelector('.submit-button');
+
+const onInputChange = () => {
+  const isValidForm = formElem.reportValidity();
+  if (isValidForm) {
+    submitBtnElem.removeAttribute('disabled');
+  } else {
+    submitBtnElem.setAttribute('disabled', true);
+  }
+};
+
+emailInput.addEventListener('input', onInputChange);
+passwordInput.addEventListener('input', onInputChange);
+nameInput.addEventListener('input', onInputChange);
+
+const baseUrl = 'https://662630a1052332d55321f809.mockapi.io/api/v1/users';
+
+const createUser = userData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
   });
+};
 
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
+const clearForm = () => {
+  emailInput.value = '';
+  passwordInput.value = '';
+  nameInput.value = '';
+};
 
-    if (!form.reportValidity()) {
-      return;
-    }
+const onError = () => {
+  errorElem.textContent = 'Failed to create user';
+};
 
-    const formData = Object.fromEntries(new FormData(form));
-    return fetch(serverUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(formElem));
+
+  createUser(formData)
+    .then(response => response.json())
+    .then(data => {
+      alert(JSON.stringify(data));
+      clearForm();
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        alert(JSON.stringify(data, null, 2));
-        form.reset();
-        submitButton.disabled = true;
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  });
-});
+    .catch(onError);
+};
+
+formElem.addEventListener('submit', onFormSubmit);
