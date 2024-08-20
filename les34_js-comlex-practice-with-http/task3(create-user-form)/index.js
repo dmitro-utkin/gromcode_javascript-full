883f0384f7c5c0dff0105b017667be4cfe355da1
ДМИТРО <sssddd1366628@gmail.com>
2024-08-20 17:42:07 +0300

    fixed code review

--- les34_js-comlex-practice-with-http/task3(create-user-form)/index.js
+++ les34_js-comlex-practice-with-http/task3(create-user-form)/index.js
@@ -1,17 +1,9 @@
 const baseUrl = 'https://662630a1052332d55321f809.mockapi.io/api/v1/users';
 
-const inputElems = document.querySelectorAll('.form-input');
 const formElem = document.querySelector('.login-form');
 const submitBtnElem = document.querySelector('.submit-button');
 
-const onInputChange = () => {
-  const isValidForm = formElem.reportValidity();
-  if (isValidForm) {
-    submitBtnElem.removeAttribute('disabled');
-  } else {
-    submitBtnElem.setAttribute('disabled', true);
-  }
-};
+const onInputChange = () => submitBtnElem.disabled = !formElem.reportValidity()
 
 formElem.addEventListener('input', onInputChange)
 
@@ -27,21 +19,13 @@ const createUserAndSubmitForm = event => {
     },
     body: JSON.stringify(formData),
   })
-    .then(response => {
-      if (response.ok) {
-        return response.json().then(data => {
-          alert(JSON.stringify(data));
-          formElem.reset();
-        });
-      } 
-      else {
-        return response.json().then(errorData => {
-          alert(`Error creating user: ${errorData.message}`);
-        });
-      }
-    })
+    .then(response => response.json())
+    .then(data => {
+      alert(JSON.stringify(data));
+      formElem.reset();
+      submitBtnElem.setAttribute('disabled', true);
+  })
     .catch(error => {
-      console.error('Error creating user:', error);
       alert('Failed to create user. Please try again later.');
     });
 };
